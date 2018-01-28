0e8d6ff210b46755ed6147d1856e12804ee8dcf1
Jeanette Amarilla <jeanette.amarilla@hotmail.com>
2018-01-28 11:40:48 -0300

    Añadir intro

---
+++ Assets/Scripts/menuController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class menuController : MonoBehaviour {
+
+	// Use this for initialization
+
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		exit ();
+	}
+
+	public void useInfoButton () {
+		SceneManager.LoadScene (2);
+	}
+
+	public void useStartButton () {
+		SceneManager.LoadScene (1);
+	}
+
+	public void useBackButton () {
+		SceneManager.LoadScene (0);
+	}
+
+
+	public void exit(){
+		
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			print ("Algo");
+			SceneManager.LoadScene (0);
+		}
+	}
+}
