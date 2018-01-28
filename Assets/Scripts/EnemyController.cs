d3ee747ec0b33a97120e70602b8937d9086e9328
Damián Nohales <damiannohales@gmail.com>
2018-01-27 23:29:58 -0300

    Add prowl to Enemy

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,20 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour 
+public class EnemyController : MonoBehaviour
 {
 	public int Influence;
 	public bool _converted;
 	public float speed;
+    public float prowlSpeed;
+    public List<Transform> prowlPath;
 
 	public PlayerController player;
 	public EnemyController enemy;
+    private int currentProwlDestinationIndex;
 	public CircleCollider2D _smallCollider;
 
 	private Rigidbody2D rb;
 
 	private void Awake()
 	{
+        if (prowlPath.Count > 0) {
+            currentProwlDestinationIndex = 0;
+        } else {
+            currentProwlDestinationIndex = -1;
+        }
 		rb = GetComponent<Rigidbody2D> ();
 
 		_converted = false;
@@ -34,30 +42,10 @@ public class EnemyController : MonoBehaviour
 		}
 	}
 
-	private void FixedUpdate()
-	{
-		if (!_converted)
-			return;
-
-		float dis = 0;
-		if (player != null)
-			dis = Vector2.Distance (gameObject.transform.position, player.gameObject.transform.position);
-		else if (enemy != null)
-			dis = Vector2.Distance (gameObject.transform.position, enemy.gameObject.transform.position);
-
-		if (dis > 3f) {
-			rb.velocity = GetVel ();
-			float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
-			transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-		} else {
-			rb.velocity = Vector3.zero;
-		}
-	}
-
 	public int Follow(PlayerController _player) {
 		if (enemy != null)
 			enemy = null;
-		
+
 		_converted = true;
 		player = _player;
 		rb.isKinematic = false;
@@ -73,19 +61,61 @@ public class EnemyController : MonoBehaviour
 		this.enemy = enemy;
 		rb.isKinematic = false;
 
-		return Influence;	
+		return Influence;
 	}
 
-	private Vector3 GetVel()
-	{
-		Vector3 vel = new Vector3 ();
-		if (player != null) {
-			vel = player.transform.position - gameObject.transform.position;
-		}
-		else if  (enemy != null)
-			vel = enemy.transform.position - gameObject.transform.position;
+    private void FixedUpdate()
+    {
+        if (_converted) {
+            UpdateMovementConverted ();
+        } else {
+            UpdateMovementProwl ();
+        }
+    }
 
-		vel.Normalize ();
-		return vel * speed;
-	}
+    private void UpdateMovementConverted()
+    {
+        float dis = 0;
+        Vector3 destination = new Vector3 ();
+        if (player != null) {
+            dis = Vector2.Distance (gameObject.transform.position, player.gameObject.transform.position);
+            destination = player.gameObject.transform.position;
+        } else if (enemy != null) {
+            dis = Vector2.Distance (gameObject.transform.position, enemy.gameObject.transform.position);
+            destination = enemy.gameObject.transform.position;
+        }
+
+        if (dis > 3f) {
+            rb.velocity = GetVel (destination, speed);
+            float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        } else {
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+    private void UpdateMovementProwl()
+    {
+        if (currentProwlDestinationIndex >= 0) {
+            Vector3 destination = prowlPath[currentProwlDestinationIndex].position;
+            float dis = Vector2.Distance (gameObject.transform.position, destination);
+            if (dis > 1f) {
+                rb.velocity = GetVel (destination, prowlSpeed);
+                float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            } else {
+                currentProwlDestinationIndex++;
+                if (currentProwlDestinationIndex == prowlPath.Count) {
+                    currentProwlDestinationIndex = 0;
+                }
+            }
+        }
+    }
+
+    private Vector3 GetVel(Vector3 destination, float speed)
+    {
+        Vector3 vel = destination - gameObject.transform.position;
+        vel.Normalize ();
+        return vel * speed;
+    }
 }
